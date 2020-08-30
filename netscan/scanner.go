41f21f40f7e6382efab78a35124bf0a143c33a11
Andrew Abwoga <andrew.abwoga@gmail.com>
2020-08-30 18:04:30 +0300

    Version Intensity...9

--- netscan/scanner.go
+++ netscan/scanner.go
@@ -56,6 +56,7 @@ func (ns NetworkScan) Service(providerResultsKey string, ipList []string, servic
 		nmap.WithPorts("80,443,27017-27020,5432,3306,6379,6380,22,2222,5984,9200,9201,5601,9300,9301,4369,8091-8096,9100-9106,9110-9118,9120-9122,9130,9999,11209-11211,21100"),
 		nmap.WithScripts(nseScripts...),
 		nmap.WithServiceInfo(),
+		nmap.WithVersionAll(),
 		nmap.WithFilterHost(func(h nmap.Host) bool {
 			for idx := range h.Ports {
 				if h.Ports[idx].Status() == "open" {
